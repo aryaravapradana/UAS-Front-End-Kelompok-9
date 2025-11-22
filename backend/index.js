36910840bc9c119916126ccb7ecd10878c6d6e82
aryaravapradana <aryaravapradana5555@gmail.com>
2025-11-22 18:36:59 +0700

    Fix: Remove package-lock.json to fix build and clean up code

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors'); // Removed unused dependency
 const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
